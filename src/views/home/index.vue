b3a275eb311b9f6fa611d2695a1433560c908116
yyshu <2671255784@qq.com>
2023-02-08 23:16:03 +0800

    引入cesium

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,19 +2,37 @@
  * @Author: yyshu 2671255784@qq.com
  * @Date: 2023-02-07 22:22:57
  * @LastEditors: yyshu 2671255784@qq.com
- * @LastEditTime: 2023-02-07 22:50:50
+ * @LastEditTime: 2023-02-08 22:59:32
  * @FilePath: \vite\vite-cesium-mapbox\src\views\home\index.vue
  * @Description: 首页
 -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { Viewer, ScreenSpaceEventHandler, ScreenSpaceEventType } from "cesium";
+
+onMounted(() => {
+  const viewer = new Viewer("box", {});
+  // 增加鼠标左键点击事件
+  let handler = new ScreenSpaceEventHandler(viewer.scene.canvas);
+  handler.setInputAction((e: ScreenSpaceEventHandler.PositionedEvent) => {
+    console.log("触发左键点击", e);
+  }, ScreenSpaceEventType.LEFT_CLICK);
+});
+</script>
 
 <template>
-  <div class="home">123</div>
+  <div id="box" class="home"></div>
 </template>
 
 <style scoped lang="scss">
 .home {
   height: 100%;
   background-color: aqua;
+}
+</style>
+
+<style>
+.cesium-viewer-bottom {
+  display: none;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+/*
+ * @Author: yyshu 2671255784@qq.com
+ * @Date: 2023-02-06 23:23:51
+ * @LastEditors: yyshu 2671255784@qq.com
+ * @LastEditTime: 2023-02-08 21:01:09
+ * @FilePath: \vite\vite-cesium-mapbox\vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import cesium from "vite-plugin-cesium";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cesium()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
